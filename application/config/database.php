3b70ac533a6b975d3abb8b7d7a956fb25ba81dd0
toeyya <clinton.toey@gmail.com>
2013-05-31 17:19:23 +0700

    gg

--- application/config/database.php
+++ application/config/database.php
@@ -37,10 +37,10 @@
 $active_group = "default";
 $active_record = TRUE;
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "root";
-$db['default']['password'] = "";
-$db['default']['database']=	 "r36";
-$db['default']['dbdriver'] = "mysql";
+$db['default']['username'] = "sa";
+$db['default']['password'] = "1234";
+$db['default']['database']=	 "R36";
+$db['default']['dbdriver'] = "mssql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
