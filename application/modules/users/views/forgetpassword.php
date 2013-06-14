964aace5148ca27a114a17212d6bb146116f4757
toeyya <clinton.toey@gmail.com>
2013-06-14 17:04:09 +0700

    forgetpassword

--- application/modules/users/views/forgetpassword.php
+++ application/modules/users/views/forgetpassword.php
@@ -1,12 +1,13 @@
+<div style="margin-left:20px;">
 <h1>ลืมรหัสผ่าน?</h1>
 <div>
 กรุณาระบุที่อยู่อีเมลของท่านในช่องด้านล่าง   เราจะส่งอีเมลเกี่ยวกับข้อมูลบัญชีของท่านไปให้ในทันที
 </div>
 <form action="users/sendmail" action="post" class="">
 <fieldset>
-	<legend>Legend</legend>
-	<label>Label name</label>
-	<input type="text" placeholder="อีเมล์">
-	 <button type="submit" class="btn btn-primary">ตกลง</button>
+	<legend>อีเมล์</legend>
+	<input type="text" placeholder="อีเมล์" class="input-large">
+	 <button type="submit" class="btn btn-small btn-primary" style="margin-bottom:10px;">ตกลง</button>
 </fieldset>
 </form>
+</div>
